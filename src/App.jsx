57b84e93b46eb70a65c4a4805f0002644653367a
sweekar <sweekardhaigude103@gmail.com>
2024-11-11 17:05:25 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/result' element={<Result/>}/>
         <Route path='/buy' element={<BuyCredit/>}/>
       </Routes>
+      <Footer/>
     </>
   )
 }
