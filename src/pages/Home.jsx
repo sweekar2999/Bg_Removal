de017aba537a24823b5d42c3a67a9eed7cb39143
sweekar <sweekardhaigude103@gmail.com>
2024-11-11 17:04:25 +0530

    Added Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
+import BgSlider from '../components/BgSlider'
+import Testimonial from '../components/Testinomial'
+import Upload from '../components/Upload'
 
 function Home() {
   return (
     <div>
-        
         <Header/>
+        <Steps/>
+        <BgSlider/>
+        <Testimonial/>
+        <Upload/>
     </div>
   )
 }
