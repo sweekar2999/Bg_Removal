08574cfecc0058cac09d845ceb3aa761f615956d
sweekar <sweekardhaigude103@gmail.com>
2024-11-29 19:59:18 +0530

    updated Result.jsx

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,37 +1,67 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Result = () => {
+  const { image, resultImage } = useContext(AppContext);
   return (
-    <div className="mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]">
-      <div className="bg-white rounded-lg px-8 py-6 drop-shadow-sm">
-     
-        <div className="flex flex-col sm:grid grid-cols-2 gap-8">
+    <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh] '>
+      <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm'>
+        <div className='flex flex-col sm:grid grid-cols-2 gap-8'>
           <div>
-            <p className="font-semibold text-gray-600 mb-2">Original</p>
-            <img className="rounded-md border" src={assets.image_w_bg} alt="" />
+            <p className='font-semibold text-gray-600 mb-2'>Orginal</p>
+            <img
+              className='rounded-md border'
+              src={image ? URL.createObjectURL(image) : ""}
+              alt='bg'
+            />
           </div>
-          <div className="flex flex-col">
-            <p className="font-semibold text-gray-600 mb-2">
+          <div className='flex flex-col'>
+            <p className='font-semibold text-gray-600 mb-2'>
               Background Removed
             </p>
-            <div className="rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden ">
-              <img src={assets.image_wo_bg} alt="" />
-              {/* <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
-                <div className='border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin'>
+            <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
+              <img src={resultImage ? resultImage : ""} />
 
+              {!resultImage && image && (
+                <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
+                  <div className='border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin'></div>
                 </div>
-              </div> */}
+              )}
             </div>
           </div>
         </div>
-              <div className='flex justify-center flex-wrap sm:justify-end items-center  gap-4 mt-6'>
-                <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-500'>Try Another Image</button>
-                <a className='px-8 py-2.5 text-sm text-white bg-gradient-to-r from-violet-600 to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-500' href="">Download Image</a>
-              </div>
+
+        {resultImage && (
+          <div className='flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6'>
+            <div>
+              <input
+                onChange={(e) => removeBG(e.target.files[0])}
+                accept='image/*'
+                type='file'
+                id='image1'
+                hidden
+              />
+              <label
+                htmlFor='image1'
+                className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700'
+              >
+                <img width={20} src={assets.upload_btn_icon} alt='upload btn' />
+                <p className='text-white text-sm'>Try another image</p>
+              </label>
+            </div>
+            <a
+              className='px-8 py-2.5 text-white text-sm bg-gradient-to-r from-violet-600 to-fuchsia-500 rounded-full  hover:scale-105 transition-all duration-700'
+              href={resultImage}
+              download
+            >
+              Download image
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
-}
- 
-export default Result
+};
+
+export default Result;
