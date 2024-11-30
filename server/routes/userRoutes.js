1e7d1e93ac9cbf9114115515c13018bddbe84424
sweekar <sweekardhaigude103@gmail.com>
2024-12-01 00:28:40 +0530

    Add Set up user routes for  Razorpay payment processing

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { clerkWebhooks, userCredits } from '../controllers/userController.js'
+import { clerkWebhooks, paymentRazorpay, userCredits, verifyRazorPay } from '../controllers/userController.js'
 import authUser from '../middlewares/auth.js'
 
 const userRouter = express.Router()
@@ -8,4 +8,6 @@ const userRouter = express.Router()
 
 userRouter.post('/webhooks',clerkWebhooks)
 userRouter.get('/credits',authUser,userCredits)
+userRouter.post("/pay-razor", authUser, paymentRazorpay);
+userRouter.post("/verify-razor", verifyRazorPay);
 export default userRouter
